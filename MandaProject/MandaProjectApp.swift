6bdfa21e8af581918ba6009c402a3c755d284ef0
PARK, YUJIN <jinee525@naver.com>
2022-12-11 13:08:33 +0900

    Initial Commit

---
+++ MandaProject/MandaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MandaProjectApp.swift
+//  MandaProject
+//
+//  Created by 박유진 on 2022/12/11.
+//
+
+import SwiftUI
+
+@main
+struct MandaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
